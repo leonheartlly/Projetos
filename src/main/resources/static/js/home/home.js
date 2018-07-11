c0550d4df566fcc7f571504def85199922f7d72b
Lucas Ferreira <lucasf.silva@outlook.com>
2018-07-10 22:38:15 -0300

    noticias pagina home

--- src/main/resources/static/js/home/home.js
+++ src/main/resources/static/js/home/home.js
@@ -4,9 +4,27 @@ $(document).ready(function() {
 	 $('.negocio').lightGallery({
 	        thumbnail:true
 	    }); 
+	 
+	 $('.modal').modal();
 //	alert($(window).height());
 //	alert($(window).width());
 });
+
+
+function modalFeed( tag, noticia){
+	
+	var date = tag.children('div').children('span:first').text();
+	var topic = tag.children('div').children('span:last').text();
+	var title = tag.children('div').children('h5').text();
+	var news = tag.children('div').children('h6').text();
+	topic = topic.toUpperCase();
+	$('.modal-title').text(title);
+	$('.modal-subtitle').text(date + ' - ' + topic);
+	$('.modal-text').text(news);
+	
+	alert(noticia.titulo);
+	
+}
 
 /**
  * Vai para a próxima posição da lista de eventos.
